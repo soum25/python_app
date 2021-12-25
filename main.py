66a255e126ae63a45489af3236fb8afa2f2918b1
soum25 <vafesoumahoro@gmail.com>
2021-12-25 16:33:31 +0100

    fix conflits

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,17 +10,3 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5550)
-=======
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello_world():  
-    return render_template('index.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5555)
->>>>>>> 527ab00488a7c072aa3ef12b19a24e0443ec37f9
